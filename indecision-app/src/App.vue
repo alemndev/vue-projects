909f80e974da7fb86f9de30befb3ca3c342c9423
alemndev <140274341+alemndev@users.noreply.github.com>
2024-07-06 10:42:08 -0500

    01 - indecision-app 🦎

---
+++ indecision-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Indecision/>
+</template>
+
+<script>
+import Indecision from '@/components/Indecision.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Indecision
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ indecision-app/src/components/Indecision.vue
@@ -0,0 +1,96 @@
+<template>
+  <img :src="image" alt="bg">
+  <div class="bg-dark"></div>
+
+  <div class="indecision-container">
+    <input v-model="question" type="text" placeholder="A question">
+    <p>Remember to end each question with a question mark (?)</p>
+  
+    <div v-if="isValidQuestion">
+      <h2>{{ question }}</h2>
+      <h1>{{ answer }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Indecision',
+  data() {
+    return {
+      question: null,
+      answer: null,
+      image: null,
+      isValidQuestion: false
+    }
+  },
+  methods: {
+    async getAnswer() {
+      this.answer = 'thinking...'
+
+      const { answer, image } = await fetch('https://www.yesno.wtf/api')
+      .then(res => res.json())
+
+      this.answer = answer;
+      this.image = image
+    }
+  },
+  watch: {
+    question(value, _oldValue) {
+      this.isValidQuestion = false
+
+      if (value.endsWith('?')) {
+        this.isValidQuestion = true
+        this.getAnswer()
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+
+</style>
