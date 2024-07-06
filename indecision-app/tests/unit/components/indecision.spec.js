bed14ae18dc598cacc15aaabdd50a78e8c799986
alemndev <140274341+alemndev@users.noreply.github.com>
2024-07-06 13:37:39 -0500

    01 - indecision-app learning testing

---
+++ indecision-app/tests/unit/components/indecision.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Indecision from '@/components/Indecision.vue'
+
+describe('Indecision component', () => {
+  // let wrapper
+  // // Each test
+  // beforeEach(() => {
+  //   wrapper = shallowMount(Indecision)
+  // })
+  // test('check', () => {
+  //   // const wrapper = shallowMount(Indecision)
+  //   // expect(wrapper.html()).toMatchSnapshot()
+  // })
+  //test('render a h2 tag', () => {
+  //  //const wrapper = shallowMount(Indecision)
+  //
+  //  // events
+  //  //wrapper.trigger('click');
+  //})
+  // Read props
+  //test('set default value', () => {
+  //  //wrapper.props()
+  // //wrapper.props('prop')
+  //})
+  // test('send props', () => {
+  //   const wrapper = shallowMount(Indecision, { props: { my_prop: '.' } })
+  // })
+})
